5dfe3a8d412862be6ec79cd36b56ee1e129b6660
Adrien140401 <91193459+Adrien140401@users.noreply.github.com>
2022-04-15 11:09:11 +0200

    Add files via upload

---
+++ ProjectWeb-Front-API/script.js
@@ -0,0 +1,21 @@
+function getInputs() {
+    console.log('test');
+    const firstname = document.getElementsByClassName("firstname").item(0).value;
+    const name = document.getElementsByClassName("name").item(0).value;
+    const email = document.getElementsByClassName("email").item(0).value;
+    const password = document.getElementsByClassName("password").item(0).value;
+    const phone = document.getElementsByClassName("phone").item(0).value;
+    console.log(firstname);
+    console.log(name);
+    console.log(email);
+    console.log(password);
+    console.log(phone);
+
+    document.getElementById
+
+    var xmlhr = new XMLHttpRequest();
+    xmlhr.open("POST", 'http://127.0.0.1:8080/register', true);
+    xmlhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xmlhr.send(`firstname=${firstname}&name=${name}&email=${email}&password=${password}&phone=${phone}`);
+}
+ 
